4d8e47fe8cdf79ee0b1aaf16520f44aa43671cd7
tuncertirnavali <mttmetu@gmail.com>
2015-12-27 22:07:17 +0200

    Added example images

--- SimplePopupProject/Assets/SimplePopup/SimplePopupExample.cs
+++ SimplePopupProject/Assets/SimplePopup/SimplePopupExample.cs
@@ -7,15 +7,14 @@ public class SimplePopupExample : MonoBehaviour {
 
     public void onDefaultPopup()
     {
-        SimplePopupManager.Instance.CreatePopup("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-        SimplePopupManager.Instance.AddButton("Lorem", delegate { Debug.Log("on clicked1"); });
-        SimplePopupManager.Instance.AddButton("ipsum", delegate { Debug.Log("on clicked2"); });
-        SimplePopupManager.Instance.AddButton("dolor", delegate { Debug.Log("on clicked1"); });
+        SimplePopupManager.Instance.CreatePopup("Are you sure you want to quit?");
+        SimplePopupManager.Instance.AddButton("Yes", delegate { Debug.Log("clicked on yes"); });
+        SimplePopupManager.Instance.AddButton("No", delegate { Debug.Log("clicked on no"); });
         SimplePopupManager.Instance.ShowPopup();
     }
     public void onCustomPopup()
     {
-        SimplePopupManager.Instance.CreatePopup("Are you sure you want to quit?", Color.white, Color.black);
+        SimplePopupManager.Instance.CreatePopup("Are you sure you want to quit?", Color.white, new Color(26 / 255.0f,30/255.0f,48/255.0f));
         SimplePopupManager.Instance.AddButton("Yes", Color.white, new Color(64/255.0f,208/255.0f,116/255.0f), delegate {
             Debug.Log("on click to yes");
         });
